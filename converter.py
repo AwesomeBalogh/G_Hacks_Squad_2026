b3d542642a5a891f80209cd2dc40d5d157806942
Sam Katevatis <samkatevatis@gmail.com>
2026-02-07 15:24:30 -0700

    function converts geo to cart

--- converter.py
+++ converter.py
@@ -1,1 +1,13 @@
 # This will convert cartesian coordinates to geospacial
+import numpy as np
+def geo_to_cart(lat, lon, height):
+    a = 6378137  # WGS84
+    e = 0.00669438
+    # Convert to radians
+    lat_rad, lon_rad = np.radians(lat), np.radians(lon)
+
+    N = a / np.sqrt(1 - (e * np.sin(lat_rad)**2))
+    X = (N + height) * np.cos(lat_rad) * np.cos(lon_rad)
+    Y = (N + height) * np.cos(lat_rad) * np.sin(lon_rad)
+    Z = ((N * (1 - e**2)) + height) * np.sin(lat_rad)
+    return [X, Y, Z]
