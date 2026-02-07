86fc6a19b9e55cf148a7f0fbd7ab640c3d531091
Sam Katevatis <samkatevatis@gmail.com>
2026-02-07 12:58:17 -0700

    comments and docstrings on the parser

--- parser.py
+++ parser.py
@@ -2,6 +2,44 @@
 #BESTPOSA,USB1,0,58.5,FINESTEERING,2209,502061.000,02000020,cdba,16809;SOL_COMPUTED,PPP,51.15043706870,-114.03067882331,1097.3462,-17.0001,WGS84,0.0154,0.0139,0.0288,"TSTR",11.000,0.000,43,39,39,38,00,00,7f,37*52483ac5
 # 
 def get_log_data(param, data):
+    """
+    Extract a specific GNSS log field from a raw data string.
+
+    This function removes the header (up to the first ';'), splits the
+    remaining data by commas, and returns the value corresponding to the
+    specified parameter.
+
+    Parameters
+    ----------
+    param : str
+        The name of the field to extract (must be one of the following keys):
+        [
+            "solution_ststus", "position_type", "lat", "lon", "height",
+            "undulation", "datum_id", "lat_std_dev", "lon_std_dev",
+            "height_std_dev", "base_station_id", "differential_age",
+            "solution_age", "num_satellites", "num_satellites_L1_E1_B1",
+            "num_satellites_multi_freq"
+        ]
+    data : str
+        The raw GNSS log string containing a header followed by comma-separated values.
+
+    Returns
+    -------
+    str
+        The string value associated with the given parameter.
+
+    Raises
+    ------
+    KeyError
+        If the provided `param` is not a valid key in KEYS.
+    IndexError
+        If the data string does not contain enough fields after splitting.
+
+    Example
+    -------
+    >>> get_log_data("lat", "GGA;SOL_COMPUTED,RTK_FIXED,51.0486,-114.0719,1048.3,...")
+    '51.0486'
+    """
     KEYS = {
         "solution_ststus": 0,
         "position_type": 1,
@@ -21,10 +59,10 @@ def get_log_data(param, data):
         "num_satellites_multi_freq": 15,
     }
 
-    data = "#BESTPOSA,USB1,0,58.5,FINESTEERING,2209,502061.000,02000020,cdba,16809;SOL_COMPUTED,PPP,51.15043706870,-114.03067882331,1097.3462,-17.0001,WGS84,0.0154,0.0139,0.0288,\"TSTR\",11.000,0.000,43,39,39,38,00,00,7f,37*52483ac5"
+    # Remove header
     if ";" in data:
         no_header = data[data.index(";"):] 
+    
+    #Split to list
     split_string = no_header[1:].split(",")
-    print(split_string[KEYS["lat"]])
-
-get_log_data(1, 1)
+    return split_string[KEYS[param]]
