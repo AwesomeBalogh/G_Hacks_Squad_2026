b778bae7a4063f55b7caa9bf59663f60c50019c2
Sam Katevatis <samkatevatis@gmail.com>
2026-02-08 09:56:33 -0700

    stuff

--- Clue_5.py
+++ Clue_5.py
@@ -11,8 +11,29 @@ def transformation(V):
     legacy_easting_distance = legacy_easting[1] - legacy_easting[0]
     legacy_northing_distance = legacy_northing[1] - legacy_northing[0]
     legacy_distance = np.sqrt(legacy_easting_distance**2 + legacy_northing_distance**2)
+    legacy_angle = np.arctan2(legacy_northing_distance, legacy_easting_distance)
 
+    current_northing_distance = current_northing[1] - current_northing[0]
+    current_easting_distance = current_easting[1] - current_easting[0]
+    current_distance = np.sqrt(current_easting_distance**2 + current_northing_distance**2)
+    current_angle = np.arctan2(current_northing_distance, current_easting_distance)
+
+    scale = current_distance / legacy_distance
+    rotation = current_angle - legacy_angle
+    rotation_deg = np.rad2deg(rotation)
+
+    s_cos = scale * np.cos(rotation)
+    s_sin = scale * np.sin(rotation)
+
+    x_rotation = legacy_easting[0] * s_cos - legacy_northing[0] * s_sin
+    y_rotation = legacy_easting[0] * s_sin - legacy_northing[0] * s_cos
+
+    x_translation = current_easting[0] - x_rotation
+    y_translation = current_northing[0] - y_rotation
+
+    N_final = scale * (V[1] * np.cos(rotation) - V[0] * np.sin(rotation))
+    E_final = scale * (V[1] * np.sin(rotation) + V[0] * np.cos(rotation))
     
 
-    return V
+    return [N_final, E_final, V[2]]
 
