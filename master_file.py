28e11b9ef0aa1f207864b28135c4daa858bb6d98
Sam Katevatis <samkatevatis@gmail.com>
2026-02-07 22:04:30 -0700

    calculate b in master file

--- master_file.py
+++ master_file.py
@@ -1,9 +1,19 @@
+import sys
+import parser
 import geo_to_global_cart
 import local_bearing_to_enu
 import local_to_global
 
-A_geo = [float(input("Input Latitude: ")), float(input("Input Longditude: ")), float(input("Input h: "))]
+# #BESTPOSA,USB1,0,51.5,FINESTEERING,2404,603797.000,03000800,cdba,18018;SOL_COMPUTED,NARROW_INT,51.07899643518,-114.13251416136,1114.2977,-16.6000,WGS84,0.0079,0.0056,0.0141,"3920",1.000,0.000,34,31,31,30,00,21,7f,37*a5cec5c7
+print("Paste log line: ")
+data = sys.stdin.read()
+A_geo = [
+    float(parser.get_log_data("lat", data)),
+    float(parser.get_log_data("lon", data)),
+    float(parser.get_log_data("height", data))
+]
 A_cart = geo_to_global_cart.geo_to_cart(A_geo[0], A_geo[1], A_geo[2])
+print(f"A is {A_cart}")
 
 # Clue 1 in bearing, distance, and delta h
 Clue_1 = [153.371958, 128.5967, -3.7103] 
@@ -21,4 +31,4 @@ B_offset_cart = local_to_global.local_to_global_cart(
     A_geo[2]
 )
 B = B_offset_cart + A_cart
-print(B)
+print(f"B is {B}")
