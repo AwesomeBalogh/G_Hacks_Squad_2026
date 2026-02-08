2f10f6299e371c43793cc3c4197e837b8bfc5b18
Sam Katevatis <samkatevatis@gmail.com>
2026-02-07 21:21:45 -0700

    added local to global

--- local_to_global.py
+++ local_to_global.py
@@ -3,15 +3,15 @@ import numpy.matlib
 import geo_to_global_cart as g2c
 import global_cart_to_geo as c2g
 
-def global_cart_to_local(X, Y, Z, phi, lamda, h):
+def local_to_global_cart(N, E, U, phi, lamda, h):
     # Build nupmy array
-    global_cart = np.array([X, Y, Z])
+    global_cart = np.array([N, E, U])
 
     # Build rotation matrix
     rotation1 = [(-1 * np.sin(phi) * np.cos(lamda)), (-1 * np.sin(phi) * np.sin(lamda)), (np.cos(phi))]
     rotation2 = [(-1 * np.sin(lamda)), (np.cos(lamda)), 0]
     rotation3 = [(np.cos(phi) * np.cos(lamda)), (np.cos(phi) * np.sin(lamda)), (np.sin(phi))]
-    rotation_matrix = [rotation1, rotation2, rotation3]
+    rotation_matrix = np.transpose([rotation1, rotation2, rotation3])
 
     # Multiply the global cartesian vector by the rotation matrix
     local_cart = np.matmul(rotation_matrix, global_cart)
@@ -20,13 +20,13 @@ def global_cart_to_local(X, Y, Z, phi, lamda, h):
 
 
 if __name__ == "__main__":
-    print("What are the global cartesian coordinates? (X, Y, Z)")
-    x = float(input("Input X: "))
-    y = float(input("Input Y: "))
-    z = float(input("Input Z: "))
-    g = c2g.cartesian_to_geodetic(x, y, z)
+    print("What are the global cartesian coordinates? (N, Y, Z)")
+    n = float(input("Input N: "))
+    e = float(input("Input E: "))
+    u = float(input("Input U: "))
+    g = c2g.cartesian_to_geodetic(n, e, u)
     p = np.radians(g[0])
     l = np.radians(g[1])
     h = g[2]
-    cart = global_cart_to_local(x, y, z, p, l, h)
+    cart = local_to_global_cart(n, e, u, p, l, h)
     print(cart)
